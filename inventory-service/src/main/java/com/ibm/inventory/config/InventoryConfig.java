98f1b211c5a53cb37eed3f1ee52b574b0fb6a245
Jorko2000 <george1102@abv.bg>
2026-02-09 18:51:53 +0200

    InventoryConfig.java

---
+++ inventory-service/src/main/java/com/ibm/inventory/config/InventoryConfig.java
@@ -0,0 +1,8 @@
+package com.ibm.inventory.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class InventoryConfig {
+    // Reserved for DB / cache / concurrency tuning
+}
